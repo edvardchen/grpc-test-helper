279a7ca773ff7484c1de983df23baf1b8c17510d
edvardchen <pigeon73101@gmail.com>
2020-02-26 12:12:54 +0800

    feat: 修改端口随机范围

--- src/randomPort.ts
+++ src/randomPort.ts
@@ -1,3 +1,11 @@
+/**
+ * 根据 wiki https://en.wikipedia.org/wiki/Ephemeral_port 推荐，我们采用 49152 to 65535
+ */
+
+const MIN = 49152;
+const MAX = 65535;
+
 export default function randomPort() {
-  return Math.floor(Math.random() * 1e4);
+  // include MIN, exclude MAX
+  return Math.floor(Math.random() * (MAX - MIN)) + MIN;
 }
